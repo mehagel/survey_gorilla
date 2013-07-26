4108d5bc669498e6e53d6f07a68c0e51d4721110
michaelchihuyho <michaelchihuyho@gmail.com>
2013-07-26 16:32:14 -0700

    Added rudimentary authorization for survey results. Currently disabled, though

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -1,6 +1,15 @@
 get '/surveys/:id/results' do
 
+  # this breaks easily with the current implementation of current_user
+  # @user = current_user
   @survey = Survey.find_by_id(params[:id])
-  # Look in app/views/index.erb
-  erb :survey_results
+
+  @user = @survey.user # comment this line out for real behavior
+
+  if @user == @survey.user
+    erb :survey_results
+  else
+    erb :not_authorized
+  end
+
 end
