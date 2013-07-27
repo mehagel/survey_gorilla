b8652c9ff80e0af21d45ac2f3826c2933c50886d
dbc-apprentice <dbc@dbc.com>
2013-07-27 15:41:46 -0700

    deleted login.erb

--- public/js/jake.js
+++ public/js/jake.js
@@ -2,15 +2,23 @@ $(document).ready(function() {
 
   $('#login').click(function(e){
     e.preventDefault();
-    console.log('hello')
-    // $('#create_user').toggleClass("slide_bar");
+    $('#login').fadeOut(500);
     $('#create_user').animate({
       left: '+=700'
     }, 800);
     $('#form_user').find('input').first().focus();
-  // $('#create_user').on('click', function(){}
-    
-  //   });
+    $('#create_user').on('click', function(e){
+      console.log(e.target)
+      if($(e.target).is('input')){
+        e.preventDefault();
+        return;
+      }else{
+      $('#create_user').animate({
+        left: '-=700'
+      }, 800);
+      $('#login').fadeIn(500);
+    };
+    });
   });
 
 
