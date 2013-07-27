da6e00405c198850a98c9e7b5edc9501d0f6d6b9
dbc-apprentice <dbc@dbc.com>
2013-07-27 13:49:45 -0700

    removed two views that no one was using

--- public/js/jake.js
+++ public/js/jake.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
   $('#login').click(function(e){
     e.preventDefault();
     console.log('hello')
-    $('#create_user').toggleClass("slide_bar");
-  //   $('#create_user').animate({
-  //     left: '+=700'
-  //   }, 800);
-  //   $('#form_user').find('input').first().focus();
-  // $('#create_user').on('click', function()
+    // $('#create_user').toggleClass("slide_bar");
+    $('#create_user').animate({
+      left: '+=700'
+    }, 800);
+    $('#form_user').find('input').first().focus();
+  // $('#create_user').on('click', function(){}
     
   //   });
   });
