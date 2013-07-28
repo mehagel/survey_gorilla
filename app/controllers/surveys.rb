15e280a3842230dea2181168ecfa1f57961109f7
michaelchihuyho <michaelchihuyho@gmail.com>
2013-07-27 17:52:08 -0700

    Starting to incorporate and test out d3

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -1,13 +1,13 @@
 get '/surveys/:id/results' do
 
   # this breaks easily with the current implementation of current_user
-  # @user = current_user
+  # @user = current_user # uncomment this line for real behavior
   @survey = Survey.find_by_id(params[:id])
 
-  @user = @survey.user # comment this line out for real behavior
+  @user = @survey.user # comment this line for real behavior
 
   if @user == @survey.user
-    erb :survey_results
+    erb :survey_results, :layout => :chart
   else
     erb :not_authorized
   end
