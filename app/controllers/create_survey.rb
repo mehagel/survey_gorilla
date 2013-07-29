0b15899b3e825d4f41db55e617dea44b6ed6f522
dbc-apprentice <dbc@dbc.com>
2013-07-28 21:54:09 -0700

    more stuff

--- app/controllers/create_survey.rb
+++ app/controllers/create_survey.rb
@@ -1,16 +1,14 @@
 get '/create_survey' do
-  # @survey = Survey.find(params[:id])
   erb :create_survey
 end
 
 post '/create_survey' do
-  p params
   survey = current_user.surveys.create(name: params[:name])
-  # params[:questions].each do |question|
-  #   the_question = survey.questions.create(content: question[1][:content])
-  #   question[1][:choice].each do |choice|
-  #     the_question.choices.create(content: choice)
-  #   end
-  # end
+  params[:questions].each do |question|
+    the_question = survey.questions.create(content: question[1][:content])
+    question[1][:choice].each do |choice|
+      the_question.choices.create(content: choice)
+    end
+  end
 end
 
